a3119283ba2f82279188e906f3896b609174654d
123-heba <hshtqsshsh@gmail.com>
2025-02-04 07:46:55 +0200

    Initial commit

--- resources/views/book/success.blade.php
+++ resources/views/book/success.blade.php
@@ -4,13 +4,13 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title></title>
+        <title> </title>
         <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+     
     
     </head>
     <body>
-        <h3>it is success</h3>
+        <h3> created successfully </h3>
      
     </body>
 </html>
